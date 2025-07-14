1aa6045ede8d6c9b3c46356656434aecb0777df5
🛠️ Bptrack | Soporte Técnico <Soporte@bptrack.net>
2025-07-13 21:18:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,15 +32,14 @@ def autenticar():
             "web": {
                 "client_id": st.secrets["google_oauth"]["client_id"],
                 "client_secret": st.secrets["google_oauth"]["client_secret"],
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_uri": st.secrets["google_oauth"]["auth_uri"],
+                "token_uri": st.secrets["google_oauth"]["token_uri"],
                 "redirect_uris": st.secrets["google_oauth"]["redirect_uris"]
             }
         },
         scopes=SCOPES,
         redirect_uri=st.secrets["google_oauth"]["redirect_uris"][0]
     )
-
     # Manejar el código de autorización
     if "code" in st.query_params:
         try:
